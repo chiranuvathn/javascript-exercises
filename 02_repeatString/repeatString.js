82748309a9ab75164c81a42e01500609f7cec440
GearChira <chiranuvathn@gmail.com>
2024-08-28 22:45:15 +0700

    Modify JavaScript files to satisfy objective of the exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, num) {
+    if (num >= 0) {
+        let string = '';
+        for (let i = 0; i < num; i++) {
+            string += word;
+        }
+        return string        
+    } else {
+        return 'ERROR'
+    }
+}
+;
 
 // Do not edit below this line
 module.exports = repeatString;
