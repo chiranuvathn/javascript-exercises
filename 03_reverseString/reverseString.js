4240fed5c3fa6923fa104af67c884fc2d380ff9a
GearChira <chiranuvathn@gmail.com>
2024-08-29 21:44:55 +0700

    Modify JavaScript files to satisfy objective of the exercises.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const splitArray = string.split('');
 
+    const reverseSplitArray = splitArray.reverse();
+
+    let reverseString = '';
+
+    for (let letter of reverseSplitArray) {
+        reverseString += letter
+    }
+
+    return reverseString
 };
 
 // Do not edit below this line
