f7e8aba54900383650fb17bb8b77f05107720bc4
GearChira <chiranuvathn@gmail.com>
2024-09-07 22:36:34 +0700

    Modify JavaScript files to satisfy objective of the exercises.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sortedNum = [num1, num2].sort((a, b) => a - b);
 
+    if (sortedNum[0] > 0 && sortedNum[1] > 0 && Number.isInteger(sortedNum[0]) && Number.isInteger(sortedNum[1])) {
+        let finalSum = 0;
+        for (let i = sortedNum[0]; i <= sortedNum[1]; i++) {
+            finalSum += i;
+        }
+        return finalSum
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
